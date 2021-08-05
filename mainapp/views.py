f10825983a47aa59ebd6e541f8ec72f9e1dc4662
machukhinktato <tarabrinmv@gmail.com>
2021-08-05 20:05:51 +0300

    poll completed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
 
 
+class IndexView(generic.ListView):
+    template_name = 'mainapp/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    return HttpResponse("Hello world, i'm there")
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'mainapp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'mainapp/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'mainapp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('mainapp:results', args=(question.id,)))
